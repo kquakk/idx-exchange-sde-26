9b8b783db6d207162ceccf5d9b42a9f4780ddfe9
kquakk <235231506+kquakk@users.noreply.github.com>
2026-08-17 11:50:23 -0400

    Updated routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ListingPage from "./pages/ListingPage";
 import PropertyDetailPage from "./pages/PropertyDetailPage";
+import FavoritesPage from "./pages/FavoritesPage";
 import "./App.css";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ListingPage />} />
-        <Route path="/property/:id" element={<PropertyDetailPage />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<ListingPage />} />
+                <Route path="/favorites" element={<FavoritesPage />} />
+                <Route path="/property/:id" element={<PropertyDetailPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
