60db5aa9479f4bf6bfeae3bab5176ec7e5e4a20a
kquakk <235231506+kquakk@users.noreply.github.com>
2026-08-24 20:53:03 -0400

    Implemented error boundary in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ErrorBoundary from "./components/ErrorBoundary";
 import ListingPage from "./pages/ListingPage";
 import PropertyDetailPage from "./pages/PropertyDetailPage";
 import FavoritesPage from "./pages/FavoritesPage";
@@ -6,13 +7,36 @@ import "./App.css";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<ListingPage />} />
-                <Route path="/favorites" element={<FavoritesPage />} />
-                <Route path="/property/:id" element={<PropertyDetailPage />} />
-            </Routes>
-        </BrowserRouter>
+        <ErrorBoundary>
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <ErrorBoundary>
+                                <ListingPage />
+                            </ErrorBoundary>
+                        }
+                    />
+                    <Route
+                        path="/favorites"
+                        element={
+                            <ErrorBoundary>
+                                <FavoritesPage />
+                            </ErrorBoundary>
+                        }
+                    />
+                    <Route
+                        path="/property/:id"
+                        element={
+                            <ErrorBoundary>
+                                <PropertyDetailPage />
+                            </ErrorBoundary>
+                        }
+                    />
+                </Routes>
+            </BrowserRouter>
+        </ErrorBoundary>
     );
 }
 
